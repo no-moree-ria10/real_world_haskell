436cf80f7c93cca1c3260ab8a9cbcb1913580196
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-18 07:52:37 +0900

    fix bug: add module Prettyfy

--- ch05/Prettyfy.hs
+++ ch05/Prettyfy.hs
@@ -1,7 +1,9 @@
 -- file: ch-5/Prettyfy.hs
 -- Prettyモジュールはtext関数とdouble関数とstring関数を提供する。Prettyfyのモジュールを使ったAPIを使ったrenderJValueの新しい定義。これを満たすAPIを作る
-import PrettyStub --TODO:スタブを本番にする
+module Prettyfy where --これつけないとmainファイルだと思われてコンパイルしてくれない。
+
 import SimpleJSON 
+import PrettyStub --TODO:スタブを本番にする
   
 renderJValue :: JValue -> Doc
 renderJValue (JBool True) = text "true"
@@ -9,3 +11,6 @@ renderJValue (JBool False) = text "false"
 renderJValue JNull = text "null"
 renderJValue (JNumber num) = double num
 renderJValue (JString str) = string str
+
+
+                             
