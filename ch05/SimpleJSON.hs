c65b001d026cf00677347a2ad8501a1412c88ed0
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-17 21:20:29 +0900

    bug fix

--- ch05/SimpleJSON.hs
+++ ch05/SimpleJSON.hs
@@ -1,6 +1,6 @@
 --file: ch05/SimpleJSON.hs
 
 data JValue = JString String | JNumber Double | JBool Bool | JNull | JObject [ ( String, JValue ) ] | JArray [JValue] 
-                                                                                                      deribing(Eq, Ord, Show)
+                                                                                                      deriving (Eq, Ord, Show)
 
 
