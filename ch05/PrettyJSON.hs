8c17702f0c6bdc368c33b0a1d74f0a817a47418e
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-18 10:25:10 +0900

    add escape Sequence

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,4 +1,10 @@
 -- file: ch05/PrettyJSON.hs
+module PrettyJSON where
+--import
+import Numeric(showHex)
+
+--data type Doc
+data Doc = ToBeDefined deriving(Show)
 --Doc文字列はクォートされた文字の列
 string:: String -> Doc
 string = enclose '"' '"' . hcat . map oneChar
@@ -15,5 +21,33 @@ char r = undefined
 --[Doc]を一つのDocに連結(concat)
 hcat ::[Doc] -> Doc
 hcat xs = undefined
+--ここの文字列をエスケープするか表示可能形式に変換
+oneChar:: Char -> Doc
+oneChar c = case lookup c simpleEscapes of Just r -> text r
+                                           Nothing | mustEscape c->hexEscape c
+                                                   | otherwise -> char c
+                                           where mustEscape c = c < ' ' || c =='\x7f' || c > '\xff' -- if c == ASCII  -> true
+--改行文字などを出力するためにchar(\n) -> String("/n")という文字列に変換するための連想配列                                               
+simpleEscapes :: [(Char, String)] 
+simpleEscapes zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+  where ch a b = (a, ['\\', b] )
+--ASCII文字以外をエスケープするための関数 
+hexEscape  
+
+--smallHex 文字コードをunicode文字に変換し、docに入れる。でも0xffffまでしか表現できない！！（0x10ffffまでほしい) 0xffff以上を表現するには、2つにビットを分割する必要がある
+smallHex::Int -> Doc
+smallHex x = text "\\u"
+             <> text ( replicate ( 4 - length h ) 'o' )
+             <> text h
+  where h = showHex x "" --showHex: 数の16進数表記を返す
+        
+--
+
+
+
+
+
+
+
 
 
