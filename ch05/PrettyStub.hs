e6c3c330995fc961aedcf3a8a4d5d14d55fba87e
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-18 11:19:02 +0900

    move stub to PrettyStub.hs from PrettyJSON

--- ch05/PrettyStub.hs
+++ ch05/PrettyStub.hs
@@ -17,3 +17,13 @@ double num = undefined --undefinedはどこの値にもなれるが、評価し�
 
 fsep ::[Doc] ->Doc
 fsep xs = undefined --Doc値のリストを合成し、出力が単一行に入りきらない場合行を折り返す。
+
+--Doc値に対する連結演算子(:)
+(<>):: Doc -> Doc -> Doc
+a <> b = undefined
+--一文字をDoc値に変換
+char:: Char -> Doc
+char r = undefined
+--[Doc]を一つのDocに連結(concat)
+hcat ::[Doc] -> Doc
+hcat xs = undefined
