fd7dfb59bc8d7b48d8da6d08f3454acd16df6f65
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-20 15:24:12 +0900

    by charClass

--- ch08/GlobRegex.hs
+++ ch08/GlobRegex.hs
@@ -19,8 +19,15 @@ globToRegex' ('[':c:cs) ='[' : c : charClass cs
 globToRegex' ('[':_) = error "unterminated character class"
 globToRegex' (c:cs) = escape c ++ globToRegex' cs
 
-escape =undefined
-charClass = undefined
+escape :: Char -> String
+escape c | c `elem` regexChars = '\\' : [c]
+         |otherwise = [c]                                 
+  where regexChars = "\\+()^$.{}]|"
+        
+charClass :: String -> String
+charClass (']':cs) = ']' : globToRegex' cs
+charClass (c:cs) = c: charClass cs
+charClass [] = error "unterminated character class"
                       
                          
 matchesGlob =undefined
