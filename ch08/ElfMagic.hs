00210c589dc22125aa74f7148a40ff779e553f2d
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-20 14:47:22 +0900

    pass compile

--- ch08/ElfMagic.hs
+++ ch08/ElfMagic.hs
@@ -2,6 +2,10 @@
 import qualified Data.ByteString.Lazy as L
 
 hasElfMagic :: L.ByteString -> Bool
-hasElfMagic c = L.take 4 content ==elfMagic
+hasElfMagic c = L.take 4 c ==elfMagic
   where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
 
+isElfFile :: FilePath -> IO Bool
+isElfFile path = do 
+  content <- L.readFile path
+  return (hasElfMagic content)
