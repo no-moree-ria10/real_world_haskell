46a9d7efdaecf1e871965915beef60a75cfc90eb
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-20 17:00:56 +0900

    complete prettify2.hs

--- ch11/Prettify2.hs
+++ ch11/Prettify2.hs
@@ -2,6 +2,7 @@
 module Prettify2 where
 
 import Prettyfy
+import QC
 import Test.QuickCheck
 
 prop_empty_id x =
@@ -9,3 +10,7 @@ prop_empty_id x =
   &&
   (x <> empty == x)
                  
+prop_char c = char c == Char c
+prop_text s = text s == if null s then Empty else Text s
+prop_line = line == Line
+prop_double_d d = double d == text (show d)
