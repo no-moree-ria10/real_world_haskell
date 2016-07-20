ab08f9e702a3a4940e5cb28a9caf5f90d3e17224
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-20 16:23:23 +0900

    ok

--- ch11/Arbitrary.hs
+++ ch11/Arbitrary.hs
@@ -4,9 +4,28 @@ module Arbitrary where
 import System.Random
 import Test.QuickCheck
 
-class Arbitrary a where --データ生成器class
-  arbitrary :: Gen a 
-  elements :: [a] -> Gen a
-  choose :: Random a => (a, a) -> Gen a
-  oneof :: [Gen a] -> Gen a
+--class Arbitrary a where --データ生成器class
+--  arbitrary :: Gen a 
+--  elements :: [a] -> Gen a
+--  choose :: Random a => (a, a) -> Gen a
+--  oneof :: [Gen a] -> Gen a
+  
+data Ternary = Yes
+             | No
+             | Unknown
+             deriving(Eq, Show)
+                      
+instance Arbitrary Ternary where
+  arbitrary  = do
+    n <- choose(0,2) :: Gen Int
+    return $ case n of 
+      0 -> Yes
+      1 -> No
+      _ -> Unknown
+      
+instance ( Arbitrary a, Arbitrary b ) => Arbitrary (a ,b) where     
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    return (x, y)
   
