d9108a7f169e5bd2992f4e5b2b9e70d53d587e07
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-20 10:15:44 +0900

    pass compile

--- ch06/BrokenClass.hs
+++ ch06/BrokenClass.hs
@@ -1,1 +1,16 @@
 -- file: ch06/BrokenClass.hs
+{-# LANGUAGE TypeSynonymInstances #-} --型パラメータを具体化したデータ型を型クラスのinstanceにするのを許す言語拡張
+{-# LANGUAGE FlexibleInstances #-}
+module BrokenClass where
+import SimpleJSON 
+import JSONClass
+
+
+instance (JSON a) => JSON [a] where
+  toJValue = undefined
+  fromJValue =undefined
+  
+instance (JSON a) => JSON [(String, a)] where
+  toJValue = undefined
+  fromJValue =undefined
+  
