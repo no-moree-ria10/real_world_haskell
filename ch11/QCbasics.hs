85fd45d687b6b489acb1da391754cb5eee51843a
no-moree-ria10 <otatakuma0714@gmail.com>
2016-07-21 19:47:40 +0900

    ok:

--- ch11/QCbasics.hs
+++ ch11/QCbasics.hs
@@ -24,7 +24,7 @@ prop_append xs ys = not (null xs) ==> not (null ys) ==>
                     head ( qsort (xs ++ ys) ) == min (minimum xs) (minimum ys)
 prop_sort_model xs = sort xs == qsort xs
 
---return []
-runTests = do 
+--return []                     
+runTests = do
   return []
-  $verboseCheckAll
+  $verboseCheckAll  :: [Bool]
